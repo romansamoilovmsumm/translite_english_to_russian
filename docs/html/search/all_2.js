ca838c2779bbac8b40c8b47ead85da2418f72511
Roman <roman.samoilov@math.msu.ru>
2024-12-29 22:37:25 +0300

    change doxyfile

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['doxygen_0',['Как использовать Doxygen',['../index.html#autotoc_md3',1,'']]]
+  ['documentation_0',['Documentation',['../index.html#autotoc_md5',1,'']]],
+  ['doxygen_1',['Как использовать Doxygen',['../index.html#autotoc_md3',1,'']]]
 ];
